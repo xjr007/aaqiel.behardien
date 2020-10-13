dcadb99ec5e66f11da45a0c2c311f61536472a10
abaaqielbehardien@gmail.com <abaaqielbehardien@gmail.com>
2020-10-13 18:32:06 +0200

    added responsiveness | not too happy with responsiveness needs revision

--- src/components/layout/navBar.js
+++ src/components/layout/navBar.js
@@ -8,6 +8,7 @@ import {
 	List,
 	ListItem,
 	ListItemText,
+	Hidden,
 } from '@material-ui/core';
 import { Home } from '@material-ui/icons';
 import NavLinks from '../../assets/msc/navLinks';
@@ -21,16 +22,20 @@ const navBar = () => {
 					<IconButton edge='start' color='inherit' aria-label='home'>
 						<Home fontSize='large' />
 					</IconButton>
-					<List component='nav' className='container_wrap'>
-						{NavLinks.map(({ title, path }) => (
-							<Link href={path} key={title} className='link_txt'>
-								<ListItem button>
-									<ListItemText primary={title} />
-								</ListItem>
-							</Link>
-						))}
-					</List>
-					<NavSideDrawer NavLinks={NavLinks} />
+					<Hidden mdDown>
+						<List component='nav' className='container_wrap'>
+							{NavLinks.map(({ title, path }) => (
+								<Link href={path} key={title} className='link_txt'>
+									<ListItem button>
+										<ListItemText primary={title} />
+									</ListItem>
+								</Link>
+							))}
+						</List>
+					</Hidden>
+					<Hidden mdUp>
+						<NavSideDrawer NavLinks={NavLinks} />
+					</Hidden>
 				</Container>
 			</Toolbar>
 		</AppBar>
